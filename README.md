e7a5ba6883a8dece324a438c57ff2b79b896c60c
Ben Coleman <benjamin.coleman@uconn.edu>
2018-08-19 17:44:56 -0400

    login without secret

--- README.md
+++ README.md
@@ -13,17 +13,17 @@
   `npm install`
 - Install server
 
-````cd server
-    npm install
-    ```
-* Start server
-``` npm run dev ```
-* Start react
-``` cd .. npm react-start ```
+`cd server`
+`npm install`
+
+- Start server
+  `npm run dev`
+- Start react
+  `cd .. npm react-start`
 
 ### Ports
+
 GraphQL - 4000
 Prisma - 4466
 Express - 3000
 React - 3001
-````
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,94 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { graphql } from "react-apollo";
+import { gql } from "apollo-boost";
+import { AUTH_TOKEN } from "../../constant";
 
 class Login extends Component {
-    state = {
-        username: '',
-        password: ''
-    }
+  state = {
+    username: "",
+    password: ""
+  };
 
-    handleSubmit = (event) => {
-		event.preventDefault()
-		this.props.login(this.state, this.props.history)
-	}
+  handleSubmit = e => {
+    e.preventDefault();
+    this._login(e);
+  };
 
-	handleUsername = (e) => {
-		this.setState({ username: e.target.value})
-	}
+  handleUsername = e => {
+    this.setState({ username: e.target.value });
+  };
 
-	handlePassword = (e) => {
-		this.setState({ password: e.target.value})
-	}
+  handlePassword = e => {
+    this.setState({ password: e.target.value });
+  };
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-					<div><input type='text' onChange={this.handleUsername} value={this.state.username} placeholder='Username' required/></div>
-					<div><input type='password' onChange={this.handlePassword} value={this.state.password} placeholder='Password' required/></div>
-                </form>
-            </div>
-        )
-    }
+  _login = async e => {
+    const { username, password } = this.state;
+    this.props
+      .loginMutation({
+        variables: {
+          username,
+          password
+        }
+      })
+      .then(result => {
+        console.log(result);
+        const token = result.data.login.token;
+        this.props.refreshTokenFn &&
+          this.props.refreshTokenFn({
+            [AUTH_TOKEN]: token
+          });
+        this.props.history.replace("/");
+        window.location.reload();
+      })
+      .catch(err => {
+        console.log("error");
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type="text"
+              onChange={this.handleUsername}
+              value={this.state.username}
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              onChange={this.handlePassword}
+              value={this.state.password}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div>
+            <input className="primary-button" type="submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Login
+const LOGIN_USER_MUTATION = gql`
+  mutation LoginMutation($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        id
+        username
+      }
+    }
+  }
+`;
+export default graphql(LOGIN_USER_MUTATION, { name: "loginMutation" })(
+  withRouter(Login)
+);
